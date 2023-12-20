c2cb1ac9c36a8bb2ed4899dcc1b7bd47c976c069
Erik2013-P <erikpinter15@gmail.com>
2023-12-21 00:02:04 +0100

    lazy loading 'system' created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'references',
+    loadChildren: () =>
+      import('./references/references.module').then((m) => m.ReferencesModule),
+  },
+  {
+    path: 'partners',
+    loadChildren: () =>
+      import('./partners/partners.module').then((m) => m.PartnersModule),
+  },
+  {
+    path: 'about',
+    loadChildren: () =>
+      import('./about/about.module').then((m) => m.AboutModule),
+  },
+  {
+    path: 'contact',
+    loadChildren: () =>
+      import('./contact/contact.module').then((m) => m.ContactModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
