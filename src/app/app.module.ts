590a1d7f6b79fa2a30e7c1b91965632f063f6ac4
Erik2013-P <erikpinter15@gmail.com>
2023-12-20 23:36:18 +0100

    very basic of hydroteamproduct website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ReferencesComponent } from './references/references.component';
+import { AboutComponent } from './about/about.component';
+import { PatnersComponent } from './patners/patners.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    ReferencesComponent,
+    AboutComponent,
+    PatnersComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
